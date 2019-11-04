b2f7776dfa182fbe80637b03e0a0447f76ca9cf9
joao96 <jvpoletti@gmail.com>
2019-11-04 20:18:16 -0300

    :lipstick: homescreen style improvement

--- src/components/HomeScreen/index.js
+++ src/components/HomeScreen/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
+import {
+  Container, Logo, TopContainer, Title, CategoriesContainer, Row, Column, TitleNews, NewsContainer,
+} from './styles';
 
-import { Logo, Container, Title } from './styles';
+import Category from '../Category';
 
 const pokeball = require('../../assets/pokeball.png');
 
@@ -9,8 +12,41 @@ const text = 'What Pokemon\nare you looking for?';
 
 const HomeScreen = () => (
   <Container>
-    <Logo source={pokeball} />
-    <Title>{text}</Title>
+    <TopContainer>
+      <Logo source={pokeball} />
+      <Title>{text}</Title>
+      {/* searchBar */}
+      <CategoriesContainer>
+        <Row>
+          <Column>
+            <Category text="Pokedex" color="#46D7AB" />
+          </Column>
+          <Column>
+            <Category text="Moves" color="#FA6555" />
+          </Column>
+        </Row>
+        <Row>
+          <Column>
+            <Category text="Abilities" color="#429BED" />
+          </Column>
+          <Column>
+            <Category text="Items" color="#FFCE4B" />
+          </Column>
+        </Row>
+        <Row>
+          <Column>
+            <Category text="Locations" color="#7C538C" />
+          </Column>
+          <Column>
+            <Category text="Type Charts" color="#B1736C" />
+          </Column>
+        </Row>
+      </CategoriesContainer>
+    </TopContainer>
+
+    <NewsContainer>
+      <TitleNews>Pokémon News</TitleNews>
+    </NewsContainer>
   </Container>
 );
 
