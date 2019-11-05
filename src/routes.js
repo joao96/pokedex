ba09106eff6c34023d63dd80fb21a948d6351577
Guilherme Lopes <gsfl3101@gmail.com>
2019-11-05 20:06:28 -0300

    Revert ":art: boilerplate for list and items"
    This reverts commit f843dee3c95e2c02e4d458a8550eb221e6b1d3c4.

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,7 @@
-import React from 'react';
-
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 
-import Icon from 'react-native-vector-icons/MaterialIcons';
-
 import HomeScreen from './components/HomeScreen';
-import ListPokedex from './containers/ListPokedex';
 
 const navigationOptions = { // remove bottom line and shadow from header
   headerStyle: {
@@ -22,24 +17,9 @@ const Routes = createAppContainer(createStackNavigator(
       screen: HomeScreen,
       navigationOptions,
     },
-    ListPokedex: {
-      screen: ListPokedex,
-      navigationOptions: {
-        headerLeft: <Icon name="arrow-back" size={28} color="#303943" />,
-        headerRight: <Icon name="menu" size={28} color="#303943" />,
-
-        headerStyle: {
-          marginHorizontal: 28,
-          marginTop: 50,
-          elevation: 0, // android
-          shadowOpacity: 0, // ios
-        },
-        headerTransparent: true,
-      },
-    },
   },
   {
-    initialRouteName: 'ListPokedex',
+    initialRouteName: 'HomeScreen',
   },
 ));
 
