f259089da03691435296d0fdb2abf680105695ed
Alex Souza <alexnsouza6@hotmail.com>
2019-11-04 23:40:35 -0300

    :art: removing px unity and adding news sections

--- src/components/Category/styles.js
+++ src/components/Category/styles.js
@@ -4,24 +4,25 @@ export const Container = styled.View`
 `;
 
 export const CategoryItem = styled.TouchableOpacity`
-  width: 155px;
-  height: 60px;
-  border-radius: 15px;
-  margin-left: 10px;
-  padding: 10px;
+  width: 155;
+  height: 60;
+  border-radius: 15;
+  margin-left: 10;
+  padding-top: 10;
   justify-content: space-between;
 `;
 
 export const CategoryText = styled.Text`
-  font-size: 15px;
-  marginTop: 10px;
-  marginLeft: 7px;
+  font-size: 15;
+  font-weight: bold;
+  marginTop: 10;
+  marginLeft: 7;
   color: #fff;
 `;
 
 export const Logo = styled.Image`
     position: absolute;
-    width: 63px;
-    height: 63px;
-    marginLeft: 90px;
+    width: 63;
+    height: 63;
+    marginLeft: 90;
 `;
