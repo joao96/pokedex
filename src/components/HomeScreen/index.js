e2ea316d733cbaddd41f03bb539f4b37cd6da43c
Guilherme Lopes <gsfl3101@gmail.com>
2019-11-05 20:06:28 -0300

    Revert ":lipstick: homescreen style improvement"
    This reverts commit 2ac23516bc84baecfc5ab6269c011cc36bafe255.

--- src/components/HomeScreen/index.js
+++ src/components/HomeScreen/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 
-import {
-  Container, Logo, TopContainer, Title, CategoriesContainer, Row, Column, TitleNews, NewsContainer,
-} from './styles';
 
-import Category from '../Category';
+import { Logo, Container, Title } from './styles';
 
 const pokeball = require('../../assets/pokeball.png');
 
@@ -12,41 +9,8 @@ const text = 'What Pokemon\nare you looking for?';
 
 const HomeScreen = () => (
   <Container>
-    <TopContainer>
-      <Logo source={pokeball} />
-      <Title>{text}</Title>
-      {/* searchBar */}
-      <CategoriesContainer>
-        <Row>
-          <Column>
-            <Category text="Pokedex" color="#46D7AB" />
-          </Column>
-          <Column>
-            <Category text="Moves" color="#FA6555" />
-          </Column>
-        </Row>
-        <Row>
-          <Column>
-            <Category text="Abilities" color="#429BED" />
-          </Column>
-          <Column>
-            <Category text="Items" color="#FFCE4B" />
-          </Column>
-        </Row>
-        <Row>
-          <Column>
-            <Category text="Locations" color="#7C538C" />
-          </Column>
-          <Column>
-            <Category text="Type Charts" color="#B1736C" />
-          </Column>
-        </Row>
-      </CategoriesContainer>
-    </TopContainer>
-
-    <NewsContainer>
-      <TitleNews>Pokémon News</TitleNews>
-    </NewsContainer>
+    <Logo source={pokeball} />
+    <Title>{text}</Title>
   </Container>
 );
 
