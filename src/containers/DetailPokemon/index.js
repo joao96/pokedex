0409cc2e8a2ad57c9c46f0880957a9f8e503a293
joao96 <jvpoletti@gmail.com>
2019-11-06 03:07:39 -0300

    :lipstick: inital tab layout for pokemon detail

--- src/containers/DetailPokemon/index.js
+++ src/containers/DetailPokemon/index.js
@@ -1,89 +1,92 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { TabView, SceneMap, TabBar } from 'react-native-tab-view';
-import { View, StyleSheet, Dimensions } from 'react-native';
 
 import {
-  Container, Name, DataContainer, TypeContainer, Type, TypeText, Logo, InfoContainer,
+  Container, Name, DataContainer,
+  TypeContainer, Type, TypeText, Logo,
+  InfoContainer, TabContainer, Tab, TabText,
 } from './styles';
 
-const FirstRoute = () => (
-  <View style={[styles.scene, { backgroundColor: '#ffffff' }]} />
-);
+const DetailPokemon = ({ navigation }) => {
+  const [tabs, setTabs] = useState([true, false, false, false]);
 
-const SecondRoute = () => (
-  <View style={[styles.scene, { backgroundColor: '#ffffff' }]} />
-);
+  const {
+    name, type1, type2, image, color,
+  } = navigation.state.params;
 
-const ThirdRoute = () => (
-  <View style={[styles.scene, { backgroundColor: '#ffffff' }]} />
-);
+  function tabTextStyle(index) {
+    if (!tabs[index]) {
+      return { opacity: 0.3, borderBottomWidth: 0 };
+    }
 
-const FourthRoute = () => (
-  <View style={[styles.scene, { backgroundColor: '#ffffff' }]} />
-);
+    return { opacity: 1, borderBottomWidth: 2 };
+  }
 
-
-class DetailPokemon extends React.Component {
-  state = {
-    index: 0,
-    routes: [
-      { key: 'about', title: 'About' },
-      { key: 'baseStats', title: 'Base Stats' },
-      { key: 'evolution', title: 'Evolution' },
-      { key: 'moves', title: 'Moves' },
-    ],
+  const handleTabPress = (active) => {
+    switch (active) {
+      case 0:
+        setTabs([true, false, false, false]);
+        break;
+      case 1:
+        setTabs([false, true, false, false]);
+        break;
+      case 2:
+        setTabs([false, false, true, false]);
+        break;
+      case 3:
+        setTabs([false, false, false, true]);
+        break;
+      default:
+        setTabs([true, false, false, false]);
+    }
   };
 
-  render() {
-    const { navigation } = this.props;
-    const {
-      name, type1, type2, image, color,
-    } = navigation.state.params;
 
-    return (
-      <Container style={{ backgroundColor: color }}>
-        <DataContainer>
-          <Name>
-            {name}
-          </Name>
-          <TypeContainer>
+  return (
+    <Container style={{ backgroundColor: color }}>
+      <DataContainer>
+        <Name>
+          {name}
+        </Name>
+        <TypeContainer>
+          <Type>
+            <TypeText>{type1}</TypeText>
+          </Type>
+          {type2 ? (
             <Type>
-              <TypeText>{type1}</TypeText>
+              <TypeText>{type2}</TypeText>
             </Type>
-            {type2 ? (
-              <Type>
-                <TypeText>{type2}</TypeText>
-              </Type>
-            ) : null}
-          </TypeContainer>
-        </DataContainer>
-        <InfoContainer>
-          <TabView
-            navigationState={this.state}
-            renderScene={SceneMap({
-              about: FirstRoute,
-              baseStats: SecondRoute,
-              evolution: ThirdRoute,
-              moves: FourthRoute,
-            })}
-            onIndexChange={(index) => this.setState({ index })}
-            style={{ borderTopRightRadius: 15, borderTopLeftRadius: 15 }}
-            renderTabBar={(props) => (
-              <TabBar
-                {...props}
-                indicatorStyle={{ backgroundColor: '#6C79DB' }}
-                style={{ backgroundColor: '#ffffff' }}
-                labelStyle={{ color: '#000', fontSize: 10 }}
-              />
-            )}
-          />
-        </InfoContainer>
-        <Logo source={image} />
-      </Container>
-    );
-  }
-}
+          ) : null}
+        </TypeContainer>
+      </DataContainer>
+      <InfoContainer>
+        <TabContainer>
+          <Tab onPress={() => { handleTabPress(0); }}>
+            <TabText style={tabTextStyle(0)}>
+            About
+            </TabText>
+          </Tab>
+          <Tab onPress={() => { handleTabPress(1); }}>
+            <TabText style={tabTextStyle(1)}>
+            Base Stats
+            </TabText>
+          </Tab>
+          <Tab onPress={() => { handleTabPress(2); }}>
+            <TabText style={tabTextStyle(2)}>
+            Evolution
+            </TabText>
+          </Tab>
+          <Tab onPress={() => { handleTabPress(3); }}>
+            <TabText style={tabTextStyle(3)}>
+            Moves
+            </TabText>
+          </Tab>
+        </TabContainer>
+      </InfoContainer>
+      <Logo source={image} />
+    </Container>
+  );
+};
 
 DetailPokemon.defaultProps = {
   type2: null,
@@ -100,10 +103,5 @@ DetailPokemon.propTypes = {
   }).isRequired,
 };
 
-const styles = StyleSheet.create({
-  scene: {
-    flex: 1,
-  },
-});
 
 export default DetailPokemon;
