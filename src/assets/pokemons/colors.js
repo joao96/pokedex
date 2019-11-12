c85524e1baaf5044ef04e368aed88115124d0243
Alex Souza <alexnsouza6@hotmail.com>
2019-11-12 20:34:05 -0300

    :art: Co-authored-by: Guilherme Lopes <gsfl3101@gmail.com> Co-authored-by: João Victor de Souza Poletti <jvpoletti@gmail.com>

---
+++ src/assets/pokemons/colors.js
@@ -0,0 +1,23 @@
+const colors = {
+  grass: '#46D7AB',
+  fire: '#FB6C6C',
+  water: '#46C5D7',
+  eletric: '#FFDC7D',
+  bug: '#A6B91A',
+  normal: '#A8A77A',
+  electric: '#F7D02C',
+  ice: '#96D9D6',
+  fighting: '#C22E28',
+  poison: '#A33EA1',
+  ground: '#E2BF65',
+  flying: '#A98FF3',
+  psychic: '#F95587',
+  rock: '#B6A136',
+  ghost: '#735797',
+  dark: '#705746',
+  dragon: '#6F35FC',
+  steel: '#B7B7CE',
+  fairy: '#D685AD',
+};
+
+export default colors;
