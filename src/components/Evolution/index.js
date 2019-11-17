21f2d4354e061d05bf7bd3ffddf46f591bf6e354
Guilhermesfl <guilherme@loopce.com>
2019-11-17 19:44:43 -0300

    :lipstick: Added evolution chain with pokemon images

--- src/components/Evolution/index.js
+++ src/components/Evolution/index.js
@@ -1,7 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import PropsType from 'prop-types';
 
-import { Container } from './styles';
+import {
+  Container, Logo, Title, EvolutionContainer, PokemonContainer, PokemonName,
+} from './styles';
 
-const Evolution = () => <Container />;
 
-export default Evolution;
+const Evolutions = ({ evolutions }) => {
+  const [pokemons, setPokemons] = useState([]);
+
+  const setEvolutions = async () => {
+    const aux = await evolutions;
+    setPokemons(aux);
+  };
+
+  const capitalize = (name) => name.charAt(0).toUpperCase() + name.slice(1);
+
+  useEffect(() => {
+    setEvolutions();
+  }, []);
+
+  return (
+    <Container>
+      <Title>Evolution Chain</Title>
+      <EvolutionContainer>
+        {
+        pokemons.length > 1 ? (
+          pokemons.map((pokemon) => (
+            <PokemonContainer>
+              <Logo source={{ uri: pokemon.image }} />
+              <PokemonName>{capitalize(pokemon.name)}</PokemonName>
+            </PokemonContainer>
+          ))
+        ) : (
+          <Logo source={{ uri: pokemons.image }} />
+        )
+      }
+      </EvolutionContainer>
+    </Container>
+  );
+};
+
+Evolutions.propTypes = {
+  evolutions: PropsType.isRequired,
+};
+
+export default Evolutions;
