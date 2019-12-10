96696d27891fd742fd4b9e4c10805699a4fb076e
Guilhermesfl <guilherme@loopce.com>
2019-12-10 17:57:18 -0300

    Correção busca async storage

--- src/containers/DetailPokemon/index.js
+++ src/containers/DetailPokemon/index.js
@@ -58,7 +58,7 @@ const DetailPokemon = ({ navigation }) => {
 
   const isFavorite = async () => {
     const Storage = JSON.parse(await AsyncStorage.getItem('favorites'));
-    if (Storage.length > 0) {
+    if (Storage) {
       if (Storage.includes(id)) {
         navigation.setParams({ isListed: true });
       } else {
